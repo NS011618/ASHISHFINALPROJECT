fbd46edad34e71c8748b32900cc87f07e4182ce6
NS011618 <ashishgolla2003@gmail.com>
2022-12-28 17:21:35 +0530

    adding ids,choice selected by voter to voter response table

--- migrations/20221228113514-add-required-ids-to-voteresponse.js
+++ migrations/20221228113514-add-required-ids-to-voteresponse.js
@@ -3,20 +3,65 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+     //adding the election id to voteresponses table
+    await queryInterface.addColumn("voteresponses", "EID", {
+      type: Sequelize.DataTypes.INTEGER,
+    });
+    await queryInterface.addConstraint("voteresponses", {
+      fields: ["EID"],
+      type: "foreign key",
+      references: {
+        table: "Elections",
+        field: "id",
+      },
+    });
+     //adding the voter id to voteresponses table
+    await queryInterface.addColumn("voteresponses", "voterID", {
+      type: Sequelize.DataTypes.INTEGER,
+    });
+    await queryInterface.addConstraint("voteresponses", {
+      fields: ["voterID"],
+      type: "foreign key",
+      references: {
+        table: "Voters",
+        field: "id",
+      },
+    });
+
+    //adding the question id to voteresponses table
+    await queryInterface.addColumn("voteresponses", "QID", {
+      type: Sequelize.DataTypes.INTEGER,
+    });
+    await queryInterface.addConstraint("voteresponses", {
+      fields: ["QID"],
+      type: "foreign key",
+      references: {
+        table: "EQuestions",
+        field: "id",
+      },
+    });
+   //adding the choice column to voteresponses table
+    await queryInterface.addColumn("voteresponses", "choice", {
+      type: Sequelize.DataTypes.INTEGER,
+    });
+    await queryInterface.addConstraint("voteresponses", {
+      fields: ["choice"],
+      type: "foreign key",
+      references: {
+        table: "Choices",
+        field: "id",
+      },
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    //removing the election id column from voteresponses table
+    await queryInterface.removeColumn("voteresponses", "EID");
+    //removing the voter id column from voteresponses table
+    await queryInterface.removeColumn("voteresponses", "voterID");
+    //removing the question id column from voteresponses table
+    await queryInterface.removeColumn("voteresponses", "QID");
+    //removing the choice column from voteresponses table
+    await queryInterface.removeColumn("voteresponses", "choice");
   }
 };
